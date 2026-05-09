885410b780e11d52310a15448e08b65565bbee1e
Tushar Burai <tusharburai@gmail.com>
2026-05-09 18:18:12 +0530

    Auto-download model from Google Drive

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -9,5 +9,11 @@ model = None
 def load_model():
     global model
     if model is None:
+        if not os.path.exists(MODEL_PATH):
+            print("Downloading model from Google Drive...")
+            os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+            import gdown
+            FILE_ID = "1S2tDM5qMhqnDgx7fMK4o5aq2uzQxFfjn"
+            gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", MODEL_PATH, quiet=False)
         model = tf.keras.models.load_model(MODEL_PATH)
     return model
