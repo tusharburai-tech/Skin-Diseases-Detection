9a4fd846636fb97bb8604eecb490b1bd994dd1e6
tusharburai-tech <Tusharburai30@gmailcom>
2026-05-30 19:02:30 +0530

    final absolute fix model path

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -5,25 +5,21 @@ from tensorflow.keras.models import load_model
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_DIR = os.path.join(BASE_DIR, "models")
 MODEL_PATH = os.path.join(MODEL_DIR, "skin_model.h5")
-print("Model path:", MODEL_PATH)
-print("Exists before load:", os.path.exists(MODEL_PATH))
-os.makedirs(MODEL_DIR, exist_ok=True)
 
 FILE_ID = "1S2tDM5qMhqnDgx7fMK4o5aq2uzQxFfjn"
 
-def download_model():
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+def load_skin_model():
+    # Download if not exists
     if not os.path.exists(MODEL_PATH):
         print("⬇️ Downloading model...")
         url = f"https://drive.google.com/uc?id={FILE_ID}"
         gdown.download(url, MODEL_PATH, quiet=False)
-    else:
-        print("✅ Model already exists")
 
-def load_skin_model():
-    download_model()
-    
-    if not os.path.exists(MODEL_PATH):
-        raise FileNotFoundError("Model still not found after download!")
+    # Debug logs
+    print("📁 Model path:", MODEL_PATH)
+    print("✅ Exists:", os.path.exists(MODEL_PATH))
 
-    print("📦 Loading model...")
+    # Load model
     return load_model(MODEL_PATH)
--- backend/predict.py
+++ backend/predict.py
@@ -2,24 +2,16 @@ from backend.model_loader import load_skin_model
 import numpy as np
 from tensorflow.keras.preprocessing import image
 
-# Load model once
 model = load_skin_model()
 
 def predict_image(img_path):
-    try:
-        img = image.load_img(img_path, target_size=(224, 224))
-        img_array = image.img_to_array(img) / 255.0
-        img_array = np.expand_dims(img_array, axis=0)
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-        preds = model.predict(img_array)
+    preds = model.predict(img_array)
 
-        predicted_class = np.argmax(preds)
-        confidence = float(np.max(preds))
-
-        return {
-            "prediction": str(predicted_class),
-            "confidence": confidence
-        }
-
-    except Exception as e:
-        return {"error": str(e)}
+    return {
+        "prediction": str(np.argmax(preds)),
+        "confidence": float(np.max(preds))
+    }
